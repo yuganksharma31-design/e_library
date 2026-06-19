6fbc1153cff5654e12f219a31debcaa01e32afd3
yuganksharma31-design <yuganksharma31@gmail.com>
2026-06-19 16:46:52 +0000

    Updated homepage, about page, reader fixes, navbar improvements and favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 "use client";
+
 import Navbar from "@/components/Navbar";
 import { Geist, Geist_Mono } from "next/font/google";
-import Link from "next/link";
 import { usePathname } from "next/navigation";
 import "./globals.css";
 
@@ -14,27 +14,16 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
-<meta
-  name="viewport"
-  content="width=device-width, initial-scale=1, maximum-scale=1"
-/>
 
 export default function RootLayout({ children }) {
-  const pathname = usePathname();
-
-  
-
   return (
     <html lang="en">
       <body
-  className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#98003A] text-white`}
->
-
-  <Navbar />
-
-  {children}
-
-</body>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#98003A] text-white`}
+      >
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
